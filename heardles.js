caa0fc0e13f4f0b69d1747b5632dfe12e7b1f2ab
Saxan Loster <yawgmoth@linkline.com>
2022-08-04 20:39:53 -0700

    add 90s r&b/hiphop

--- heardles.js
+++ heardles.js
@@ -1,13 +1,14 @@
 const heardles = [
-	[ 'Original'     ,  'https://www.spotify.com/heardle/' ],
-	[ '60s'          ,  'https://www.60s.heardledecades.com/' ],
-	[ '70s'          ,  'https://www.70s.heardledecades.com/' ],
-	[ '80s'          ,  'https://heardle80s.com/' ],
-	[ '80s #1s'      ,  'https://www.80s.heardledecades.com/' ],
-	[ '90s'          ,  'https://www.90s.heardledecades.com/' ],
-	[ '2000s'        ,  'https://2000sheardle.glitch.me/' ],
-	[ 'Pop'          ,  'https://popheardle.glitch.me/' ],
-	[ 'Rhythm Games' ,  'https://rhythmgame-heardle.glitch.me/' ],
+	[ 'Original' , 'https://www.spotify.com/heardle/' ],
+	[ '60s' , 'https://www.60s.heardledecades.com/' ],
+	[ '70s' , 'https://www.70s.heardledecades.com/' ],
+	[ '80s' , 'https://heardle80s.com/' ],
+	[ '80s #1s' , 'https://www.80s.heardledecades.com/' ],
+	[ '90s' , 'https://www.90s.heardledecades.com/' ],
+	[ '90s R&B/Hip-Hop' , 'https://ya-heardle.glitch.me/' ],
+	[ '2000s' , 'https://2000sheardle.glitch.me/' ],
+	[ 'Pop' , 'https://popheardle.glitch.me/' ],
+	[ 'Rhythm Games' , 'https://rhythmgame-heardle.glitch.me/' ],
 ];
 
 const list = document.querySelector( '#list' );
